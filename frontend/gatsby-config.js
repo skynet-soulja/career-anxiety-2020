f0ab817d26d61ac04fe401502d6b30398e55b70a
skynet-soulja <martinwillnorman@protonmail.com>
2021-01-15 22:57:55 -0500

    frontend: change manifest name

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -49,8 +49,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "career-anxiety",
+        short_name: "career-anxiety",
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
